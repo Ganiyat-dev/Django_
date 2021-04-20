364d3077e5e5801553a5ea43dd2aabd0616f5b81
Ganiyat-dev <ganiyatolajumoke.go@gmail.com>
2021-04-20 10:38:41 +0100

    little changes

--- ourapp/views.py
+++ ourapp/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("My Zuri First Django Application")
+    return HttpResponse("Yayyy it's My First Django Application on Zuri")
